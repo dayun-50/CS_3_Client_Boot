350cbf999a35e4051322725c8e0ec4462bafddd9
heybin <tlszn10@naver.com>
2025-11-24 18:36:11 +0900

    건강기록 출력 완

--- src/main/java/com/kedu/project/healthy_record/HealthyRecordController.java
+++ src/main/java/com/kedu/project/healthy_record/HealthyRecordController.java
@@ -1,12 +1,31 @@
 package com.kedu.project.healthy_record;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("")
+import com.kedu.project.baby.BabyDTO;
+import com.kedu.project.user.UserDTO;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RequestMapping("/checkList")
 @RestController
 public class HealthyRecordController {
     @Autowired
     private HealthyRecordService healthyRecordService;    
+
+    @GetMapping("/getBabyData")
+    public ResponseEntity<BabyDTO> getBabyData(@RequestParam("baby_seq") int babySeq, @AuthenticationPrincipal String id) {
+        System.out.println("앵"+id);
+        System.out.println("앵"+babySeq);
+        System.out.println("앵"+healthyRecordService.getBabyData(babySeq, id));
+        System.out.println("=======================");
+        return ResponseEntity.ok(healthyRecordService.getBabyData(babySeq, id));
+    }
+    
 }
--- src/main/java/com/kedu/project/healthy_record/HealthyRecordService.java
+++ src/main/java/com/kedu/project/healthy_record/HealthyRecordService.java
@@ -3,8 +3,26 @@ package com.kedu.project.healthy_record;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kedu.project.baby.BabyDAO;
+import com.kedu.project.baby.BabyDTO;
+import com.kedu.project.user.UserDAO;
+
 @Service
 public class HealthyRecordService {
     @Autowired
     private HealthyRecordDAO dao;    
+
+    @Autowired
+    private UserDAO userdao;
+
+    @Autowired
+    private BabyDAO babydao;
+
+    public BabyDTO getBabyData(int babySeq, String id){
+        BabyDTO dto = new BabyDTO();
+        dto.setFamily_code(userdao.familyCode(id));
+        dto.setBaby_seq(babySeq);
+
+        return babydao.babyMypage(dto);
+    }
 }

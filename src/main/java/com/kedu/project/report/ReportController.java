ca6dafb40bdba7144b359c59f6a01c2362ffb7e8
heybin <tlszn10@naver.com>
2025-12-01 15:02:56 +0900

    클라이언트알람기능완료

---
+++ src/main/java/com/kedu/project/report/ReportController.java
@@ -0,0 +1,10 @@
+package com.kedu.project.report;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("")
+@RestController
+public class ReportController {
+    
+}
---
+++ src/main/java/com/kedu/project/report/ReportDAO.java
@@ -0,0 +1,6 @@
+package com.kedu.project.report;
+
+public class ReportDAO {
+
+    
+}
